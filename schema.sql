f5291af672a29daee38343cdf9b919423685fb96
unorji kingsley <unorjikingsley@gmail.com>
2023-07-10 19:08:43 +0100

    modified schema file

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
-    name varchar NOT NULL,
+    name varchar(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
